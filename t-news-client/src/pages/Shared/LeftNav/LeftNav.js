ef11a57f64984fbb8f7541eb8397ae78dc0fb73d
Abir Hasan <cse.170201063@gmail.com>
2022-10-20 00:33:18 +0600

    60.6 Create side components with data from the server.

--- t-news-client/src/pages/Shared/LeftNav/LeftNav.js
+++ t-news-client/src/pages/Shared/LeftNav/LeftNav.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
     const [categories, setCategories] = useState([])
-    
+    useEffect(() => {
+        fetch("http://localhost:1000/news-categories")
+          .then((res) => res.json())
+          .then((data) => setCategories(data));
+    },[])
     return (
+      <div>
+        <h4>All Category: {categories.length}</h4>
         <div>
-            <h4>All Category</h4>
+          {categories.map((category) => (
+            <p key={category.id}>
+              <Link to={`/category/${category.id}`}> {category.name}</Link>
+            </p>
+          ))}
         </div>
+      </div>
     );
 };
 
