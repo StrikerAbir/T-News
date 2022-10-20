6bc8361eb136cb7f6549ce1fb8b63648d5d87b03
Abir Hasan <cse.170201063@gmail.com>
2022-10-21 00:09:02 +0600

    61.6 Set Auth context and auth provider to share auth info

--- t-news-client/src/pages/Shared/Header/Header.js
+++ t-news-client/src/pages/Shared/Header/Header.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
 import LeftNav from '../LeftNav/LeftNav';
 
 const Header = () => {
+  const { user } = useContext(AuthContext);
     return (
       <Navbar
         className="mb-4"
@@ -17,13 +19,19 @@ const Header = () => {
       >
         <Container>
           <Navbar.Brand>
-            <Link to='/'>T-News</Link>
+            <Link to="/">T-News</Link>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link href="#features">All News</Nav.Link>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">{ user?.displayName}</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link>
             </Nav>
             <div className="d-lg-none">
               <LeftNav></LeftNav>
