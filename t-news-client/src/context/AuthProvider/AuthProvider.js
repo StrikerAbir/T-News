0497437adc72128403ad009b59224fc74344ce72
Abir Hasan <cse.170201063@gmail.com>
2022-10-21 02:42:44 +0600

    61.7 Implement google sign in and display user info

--- t-news-client/src/context/AuthProvider/AuthProvider.js
+++ t-news-client/src/context/AuthProvider/AuthProvider.js
@@ -1,18 +1,33 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from "react";
+import app from "../../firebase/firebase.init";
+import { getAuth, onAuthStateChanged, signInWithPopup } from "firebase/auth";
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const providerLogin = (provider) => {
+    return signInWithPopup(auth, provider);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
     
-    const user = { displayName: ' bane ' };
-    const authInfo = { user };
-    return (
-        <div>
-            <AuthContext.Provider value={authInfo}>
-                {children}
-            </AuthContext.Provider>
-        </div>
-    );
+    const authInfo = { user, providerLogin };
+    
+  return (
+    <div>
+      <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+    </div>
+  );
 };
 
 export default AuthProvider;
--- t-news-client/src/pages/Shared/Header/Header.js
+++ t-news-client/src/pages/Shared/Header/Header.js
@@ -1,8 +1,10 @@
 import React, { useContext } from 'react';
+import { Image } from 'react-bootstrap';
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { FaUser } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
 import LeftNav from '../LeftNav/LeftNav';
@@ -27,10 +29,18 @@ const Header = () => {
               <Nav.Link href="#features">All News</Nav.Link>
               <Nav.Link href="#pricing">Pricing</Nav.Link>
             </Nav>
-            <Nav>
-              <Nav.Link href="#deets">{ user?.displayName}</Nav.Link>
+            <Nav className="d-flex align-items-center">
+              <Nav.Link href="#deets">{user?.displayName}</Nav.Link>
               <Nav.Link eventKey={2} href="#memes">
-                Dank memes
+                {user.photoURL ? (
+                  <Image
+                    style={{ height: "30px" }}
+                    roundedCircle
+                    src={user.photoURL}
+                  ></Image>
+                ) : (
+                  <FaUser></FaUser>
+                )}
               </Nav.Link>
             </Nav>
             <div className="d-lg-none">
