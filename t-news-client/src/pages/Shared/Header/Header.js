99f9e6efe0f7c2c52b1b2a288ce67e83db736c21
Abir Hasan <cse.170201063@gmail.com>
2022-10-20 13:21:42 +0600

    60.8 NavBar with responsive content toggle

--- t-news-client/src/pages/Shared/Header/Header.js
+++ t-news-client/src/pages/Shared/Header/Header.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import LeftNav from '../LeftNav/LeftNav';
 
 const Header = () => {
     return (
-        <div>
-            <h2>Header</h2>
-        </div>
+      <Navbar
+        className="mb-4"
+        collapseOnSelect
+        expand="lg"
+        bg="light"
+        variant="light"
+      >
+        <Container>
+          <Navbar.Brand href="#home">T-News</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">All News</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+            <div className="d-lg-none">
+              <LeftNav></LeftNav>
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     );
 };
 
